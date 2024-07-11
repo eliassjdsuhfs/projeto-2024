b2cffc3d81884bc8adda5ee9307b440de90f0952
eliasfrizon <156839232+eliassjdsuhfs@users.noreply.github.com>
2024-07-11 19:25:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var numero = 0
+function somar(){
+   numero = numero + 1
+   document.getElementById("button").innerHTML = numero
+
+
+}
+
+var nome = "Elias Frizon"
+document.getElementById("paragrafo").innerHTML = "site desenvolvido por: " + nome 
